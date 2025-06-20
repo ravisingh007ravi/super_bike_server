3d83b10ccd9f235fb715a2829a6b5c5502d57a10
ravi singh <ravisingh007ravi@gmail.com>
2025-06-20 14:21:09 +0530

    update

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,23 +1,61 @@
-const UserModel = require('../Model/userModel')
+const userModel = require('../Model/userModel');
+const UserModel = require('../Model/userModel');
 
 exports.CreateUser = async (req, res) => {
     try {
         const data = req.body;
+        console.log(data)
+        if (Object.keys(data).length === 0) return res.status(400).send({ status: false, msg: 'Data is empty' })
+     
+
+        const validationRules = {
+            name: { required: true, regex: /^[A-Za-z ]+$/, errorMsg: 'Invalid Name!' },
+            email: { required: true, regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, errorMsg: 'Invalid Email!' },
+            password: { required: true, regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, errorMsg: 'Invalid Password!' }
+        };
+
+
+        for (const [key, value] of Object.entries(data)) {
+            if (validationRules[key].required && !value) {
+                return res.status(400).send({ status: false, msg: validationRules[key].errorMsg });
+            }
+            if (validationRules[key].regex && !validationRules[key].regex.test(value)) {
+                return res.status(400).send({ status: false, msg: validationRules[key].errorMsg });
+            }
+        }
+
+
+
+        const CheckUser = await userModel.findOne({ email: email })
+        if (CheckUser) return res.status(400).send({ status: false, msg: 'Email Already Exists!' })
 
         const DB = await UserModel.create(data)
 
-        res.status(200).send({ status: true, msg: `hello ${data.name}`, data: DB })
+        res.status(201).send({ status: true, msg: 'successfull Create User', data: DB })
     }
     catch (e) { res.status(500).send({ status: false, msg: e.message }) }
 }
 
 
-exports.getAllData = async (req, res) => {
+exports.getAllData = async (_, res) => {
     try {
 
-        const DB = await UserModel.find()
+        const DB = await UserModel.find().select({ email: 1, name: 1, password: 1, _id: 0 }).sort({ createdAt: -1 })
 
-        res.status(200).send({ status: true, data: DB })
+        if (!DB) return res.status(400).send({ status: false, msg: 'Data Not Found' })
+
+        return res.status(200).send({ status: true, data: DB })
+    }
+    catch (e) { res.status(500).send({ status: false, msg: e.message }) }
+}
+
+
+exports.getUserById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const DB = await UserModel.findById(id)
+        if (!DB) return res.status(400).send({ status: false, msg: 'Data Not Found' })
+        return res.status(200).send({ status: true, data: DB })
     }
     catch (e) { res.status(500).send({ status: false, msg: e.message }) }
 }

eaf19211df9d9cdcf99ee3c7fbf64ca997746281
ravi singh <ravisingh007ravi@gmail.com>
2025-06-23 07:55:37 +0530

    updated

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,37 +1,35 @@
 const userModel = require('../Model/userModel');
-const UserModel = require('../Model/userModel');
+const bcrypt = require('bcrypt');
 
 exports.CreateUser = async (req, res) => {
     try {
         const data = req.body;
-        console.log(data)
-        if (Object.keys(data).length === 0) return res.status(400).send({ status: false, msg: 'Data is empty' })
-     
 
-        const validationRules = {
+        const userValidationRules = {
             name: { required: true, regex: /^[A-Za-z ]+$/, errorMsg: 'Invalid Name!' },
             email: { required: true, regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, errorMsg: 'Invalid Email!' },
             password: { required: true, regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, errorMsg: 'Invalid Password!' }
         };
 
+        if (Object.keys(data).length === 0) { return res.status(400).send({ status: false, msg: 'Data is empty' }); }
 
-        for (const [key, value] of Object.entries(data)) {
-            if (validationRules[key].required && !value) {
-                return res.status(400).send({ status: false, msg: validationRules[key].errorMsg });
-            }
-            if (validationRules[key].regex && !validationRules[key].regex.test(value)) {
-                return res.status(400).send({ status: false, msg: validationRules[key].errorMsg });
-            }
-        }
+        const validationErrors = Object.keys(userValidationRules).map(field => {
+            if (userValidationRules[field].required && !data[field]) { return `${field} is Required!`; }
+            if (data[field] && !userValidationRules[field].regex.test(data[field])) { return userValidationRules[field].errorMsg; }
+            return null;
+        }).filter(error => error !== null);
 
+        if (validationErrors.length > 0) { return res.status(400).send({ status: false, msg: validationErrors[0] }); }
 
+        const CheckUser = await userModel.findOne({ email: data.email });
+        if (CheckUser) return res.status(400).send({ status: false, msg: 'Email Already Exists!' });
 
-        const CheckUser = await userModel.findOne({ email: email })
-        if (CheckUser) return res.status(400).send({ status: false, msg: 'Email Already Exists!' })
+        data.password = await bcrypt.hash(data.password, 10);
 
-        const DB = await UserModel.create(data)
+        const DB = await userModel.create(data);
 
-        res.status(201).send({ status: true, msg: 'successfull Create User', data: DB })
+        res.status(201).send({ status: true, msg: 'successfull Create User', data: DB });
+
     }
     catch (e) { res.status(500).send({ status: false, msg: e.message }) }
 }
